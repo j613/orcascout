06caea2944ef6d16ea6229465669f9d576684ad7
Emerson Jacobson <emwjacobson@gmail.com>
2018-02-21 17:10:11 -0800

    Moved header html into component

--- src/app/templates/header/header.component.ts
+++ src/app/templates/header/header.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+  styleUrls: ['./header.component.less'],
+  template: `
+  <h2>
+    <b>{{ title }}</b>
+  </h2>
+  <hr>
+  `
 })
 export class HeaderComponent implements OnInit {
   @Input() title: string;
-  
+
   constructor() { }
 
   ngOnInit() {
