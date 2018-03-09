d6a96c38bf61dd33b8147857944ef3d2e40d9349
Ethan Ferguson <ethanf108@gmail.com>
2018-03-08 16:22:20 -0800

    Actually merged this time
    whoops acidentally deleted some changes from Backend-Master-Work

--- src/com/orca/backend/server/InputHandler.java
+++ src/com/orca/backend/server/InputHandler.java
@@ -1,5 +1,6 @@
 package com.orca.backend.server;
 
+import com.orca.backend.launch.Prefs;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -10,10 +11,12 @@ public interface InputHandler {
 
     public default void sendFile(ResponseFile file, String responseMessage, HashMap<String, String> args,
             BufferedWriter outWriter, Set<String> cookies) throws IOException {
-        //outWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         outWriter.write("HTTP/1.1 " + responseMessage + "\r\n");
         outWriter.write("Date: " + Utils.getHTTPDate() + "\r\n");
-            outWriter.write("Content-Length: " + (file==null?0:file.getFileLength()) + "\r\n");
+        outWriter.write("Content-Length: " + (file == null ? 0 : file.getFileLength()) + "\r\n");
+        outWriter.write("Access-Control-Allow-Origin: " + Prefs.getString("host_domain") + ", *\r\n");
+        outWriter.write("Access-Control-Expose-Headers: X-Error-Code, Content-Length, Cookie, Origin");
+        outWriter.write("Access-Control-Allow-Credentials: true");
         if (file != null) {
             outWriter.write("Content-Type: " + file.getContentType() + "\r\n");
             outWriter.write("Content-Encoding: identity\r\n");
